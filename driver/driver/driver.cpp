9d22b8fa5636ae0350ad5e9a27a6e27d5bbc847b
Gal Shav-Artza <galshav@gmail.com>
2019-08-31 00:20:15 +0300

    remove inf file.

--- driver/driver/driver.cpp
+++ driver/driver/driver.cpp
@@ -8,5 +8,10 @@ NTSTATUS DriverEntry(
 	UNREFERENCED_PARAMETER(DriverObject);
 	UNREFERENCED_PARAMETER(RegistryPath);
 
+	//UNICODE_STRING deviceName;
+	//RtlInitUnicodeString(&deviceName, L"\\Device\\filter-driver");
+	//PDEVICE_OBJECT deviceObject;
+	//IoCreateDevice(DriverObject, 0, &deviceName, FILE_DEVICE_UNKNOWN, DO_BUFFERED_IO, FALSE, &deviceObject);
+
 	return STATUS_SUCCESS;
 }
